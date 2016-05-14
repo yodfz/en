5ccf2415bd483760131fb85b989254bb094fa766
yodfz <yodfz@qq.com>
2016-05-14 23:20:47 +0800

    增加 VUE-ROUTER VUE-RESOURCE

--- app/config.js
+++ app/config.js
@@ -13,4 +13,4 @@ module.exports = {
     port: 8080,
     proxyTable: {}
   }
-}
+};
---
+++ app/src/config.js
@@ -0,0 +1,13 @@
+const localhost = '';
+
+const user = {
+    /* 用户登陆接口 */login: localhost + '',
+    /* 用户登陆界面初始化数据 */getData:localhost +''
+};
+const course = {
+    /* 课程列表 */list: localhost + ''
+};
+
+export default {
+    user, course
+};
