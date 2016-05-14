a28ee1e7907da7257d23ede34653553acbd80f0a
yodfz <yodfz@qq.com>
2016-05-15 00:45:54 +0800

    fix bug

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -8,9 +8,13 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+      // allow paren-less arrow functions
+      'arrow-parens': 0,
+      'semi': [2, "always"],
+      // allow debugger during development
+      'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+      "no-spaced-func": 2,
+      "arrow-spacing": 0,
+      "space-before-function-paren": [0, "always"]//函数定义时括号前面要不要有空格
   }
-}
+};
--- app/src/main.js
+++ app/src/main.js
@@ -1,25 +1,21 @@
+// 公共组件
 import Vue from 'vue';
 import App from './App';
-var VueRouter = require('vue-router');
+import VueRouter from 'vue-router';
+// UI组件
 
+// 引用初始化
 Vue.use(VueRouter);
 Vue.use(require('vue-resource'));
-/* eslint-disable no-new */
+
 // 创建一个路由器实例
 // 创建实例时可以传入配置参数进行定制，为保持简单，这里使用默认配置
 var router = new VueRouter();
-
 // 定义路由规则
 // 每条路由规则应该映射到一个组件。这里的“组件”可以是一个使用 Vue.extend
 // 创建的组件构造函数，也可以是一个组件选项对象。
 // 稍后我们会讲解嵌套路由
 router.map({
-    '/foo': {
-        component: Foo
-    },
-    '/bar': {
-        component: Bar
-    }
 });
 
 // 现在我们可以启动应用了！
